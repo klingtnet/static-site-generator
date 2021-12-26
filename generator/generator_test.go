c3ea1320150db2fd4357104fe524b01a6ecec5cd
Andreas Linz <klingt.net@gmail.com>
2021-12-26 12:50:31 +0100

    Implement a minimal integration test for the generator

---
+++ generator/generator_test.go
@@ -0,0 +1,92 @@
+package generator
+
+import (
+	"context"
+	"io"
+	"io/fs"
+	"sync"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"github.com/klingtnet/static-site-generator/generator/renderer"
+	"github.com/klingtnet/static-site-generator/internal/testutils"
+	"github.com/klingtnet/static-site-generator/slug"
+	"github.com/stretchr/testify/require"
+	"github.com/yuin/goldmark"
+	emoji "github.com/yuin/goldmark-emoji"
+	"github.com/yuin/goldmark/extension"
+)
+
+type memoryStorage struct {
+	t     *testing.T
+	lock  sync.Mutex
+	memFS fstest.MapFS
+}
+
+func (ms *memoryStorage) Store(_ context.Context, name string, content io.Reader) error {
+	data, err := io.ReadAll(content)
+	if err != nil {
+		return err
+	}
+
+	ms.lock.Lock()
+	ms.memFS[name] = &fstest.MapFile{
+		Data:    data,
+		ModTime: time.Now(),
+	}
+	ms.lock.Unlock()
+
+	return nil
+}
+
+func TestGenerator(t *testing.T) {
+	// Idea:
+	// If this setup is used multiple times inside tests then
+	// the generator setup could be moved into a testutil function
+	// with the following signature:
+	// NewTestGenerator(*testing.T, sourceFS, staticFS fs.FS, storage Storage)
+	contentFS := testutils.NewTestContentFS(t)
+	memStor := &memoryStorage{t: t, memFS: make(fstest.MapFS)}
+	slugifier := slug.NewSlugifier('-')
+	templates := renderer.NewTemplates("Andreas Linz", "https://klingt.net", slugifier, DefaultTemplateFS())
+	renderer := renderer.NewMarkdown(goldmark.New(goldmark.WithExtensions(extension.GFM, emoji.Emoji, extension.Footnote)), templates)
+
+	generator := New(contentFS, nil, memStor, slugifier, renderer)
+	err := generator.Run(context.Background())
+	require.NoError(t, err)
+
+	files, folders := []string{}, []string{}
+	fs.WalkDir(memStor.memFS, ".", func(path string, d fs.DirEntry, err error) error {
+		require.NoError(t, err)
+
+		if d.IsDir() {
+			folders = append(folders, path)
+		} else {
+			files = append(files, path)
+		}
+
+		return nil
+	})
+	require.ElementsMatch(t, []string{
+		".",
+		"blog",
+		"files",
+		"static",
+	}, folders)
+	require.ElementsMatch(t, []string{
+		"about.html",
+		"index.html",
+		"files/random.txt",
+		"static/base.css",
+		"blog/index.html",
+		"blog/first-article.html",
+		"blog/second-article.html",
+	}, files)
+
+	// TODO:
+	// Find an efficient and reliable way to verify the generated files' contents.
+	// Comparing against a set of golden files or doing a hash comparison of the contents.
+	// Golden files had the advantage of being easy to edit and review but come with the disadvantage of being tedious to maintain.
+	// Hashes are opaque but easy to generate and maintain.
+}
