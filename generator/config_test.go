80a52a0d0986ea759110d854e71c0c874f5e7be8
Andreas Linz <klingt.net@gmail.com>
2022-01-02 15:15:09 +0100

    Add unsafe_html to example config

--- generator/config_test.go
+++ generator/config_test.go
@@ -13,12 +13,13 @@ func TestExampleConfig(t *testing.T) {
 	config, err := ParseConfigFile("../config.example.json")
 	require.NoError(t, err, "parsing example config failed")
 	require.Equal(t, config, &Config{
-		Author:       "John Doe",
-		BaseURL:      "https://john.doe",
-		ContentDir:   "./content",
-		StaticDir:    "/optional/static",
-		OutputDir:    "./output",
-		TemplatesDir: "/optional/templates",
+		Author:           "John Doe",
+		BaseURL:          "https://john.doe",
+		ContentDir:       "./content",
+		StaticDir:        "/optional/static",
+		OutputDir:        "./output",
+		TemplatesDir:     "/optional/templates",
+		EnableUnsafeHTML: true,
 	})
 }
 
