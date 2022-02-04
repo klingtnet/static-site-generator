5be2ec41127f416a2984c13f3be4e6d5303d8f88
Andreas Linz <klingt.net@gmail.com>
2022-02-04 20:54:51 +0100

    Test menu generation for sub trees

--- generator/model/menu_test.go
+++ generator/model/menu_test.go
@@ -12,12 +12,34 @@ func TestMenu(t *testing.T) {
 	content, err := NewContentTree(context.Background(), testutils.NewTestContentFS(t), ".")
 	require.NoError(t, err)
 
-	require.ElementsMatch(t,
-		[]MenuEntry{
-			{Title: "Home", Path: "index.md"},
-			{Title: "About", Path: "about.md"},
-			{Title: "Blog", Path: "blog", IsDir: true},
-		},
+	rootMenu := []MenuEntry{
+		{Title: "Home", Path: "index.md"},
+		{Title: "About", Path: "about.md"},
+		{Title: "Blog", Path: "blog", IsDir: true},
+	}
+	require.ElementsMatch(
+		t,
+		rootMenu,
 		Menu(content),
+	)
+
+	var blog *ContentTree
+	for _, child := range content.Children() {
+		ct, ok := child.(*ContentTree)
+		if !ok {
+			continue
+		}
+
+		if ct.Path() == "blog" {
+			blog = ct
+		}
+	}
+	require.ElementsMatch(
+		t,
+		[]MenuEntry{
+			{Title: "First Article", Path: "blog/first.md"},
+			{Title: "Second Article", Path: "blog/second.md"},
+		},
+		Menu(blog),
 	)
 }
